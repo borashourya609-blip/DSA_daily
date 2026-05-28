38b4061c7f942cf55802f5789bed58118a4ca02e
borashourya609-blip <borashourya609@gmail.com>
2026-05-28 22:57:56 +0530

    first commit

---
+++ variables/4.cpp
@@ -0,0 +1,12 @@
+// taking input 
+#include <iostream>
+using namespace std ;
+
+int main() {
+    int age ;
+    cout << "enter the age : ";
+    cin >> age ;
+    cout << "you entered the age : " << age << endl ;
+    return 0;
+
+}
