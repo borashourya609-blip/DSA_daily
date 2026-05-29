60c7d82df47eb70e2f4dd9f771889cff97542507
borashourya609-blip <borashourya609@gmail.com>
2026-05-29 16:13:20 +0530

    daily commit

---
+++ operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a=10 , b=5;
+    cout << "sum is = " << (a+b) << endl;
+    cout << "difference is = " << (a-b) << endl;
+    cout << "product is = " << (a*b) << endl;
+    cout << "division is = " << (a/b) << endl ;
+    return 0;
+}
