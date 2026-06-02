b5389ddbc1fbd3d82ad8bb34a5e28251df010ebb
borashourya609-blip <borashourya609@gmail.com>
2026-06-02 21:34:21 +0530

    daily commit

--- loops/oddsum.cpp
+++ loops/oddsum.cpp
@@ -2,14 +2,14 @@
 #include<iostream>
 using namespace std;
 int main (){
-    int n = 5;
-    int sum = 1;
+    int n = 10;
+    int sum = 0;
 
     for (int i = 1 ; i <= n; i++ ){
         if (i%2 != 0){
-
+            sum += i;
         }
-        sum += i;
+        
     }
     cout << "sum is = " << sum << endl ;
     return 0;
