581bd8ff1dbaa44f3078d8d55c3e7e4ceaf2f028
borashourya609-blip <borashourya609@gmail.com>
2026-06-10 23:55:56 +0530

    daily commit

---
+++ functions/evenorodd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int n)
+{
+    return n % 2 == 0;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    if(isEven(num))
+        cout << "Even";
+    else
+        cout << "Odd";
+
+    return 0;
+}
