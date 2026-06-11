6de4aa664f7223fcabd4495aa9b013383188de90
borashourya609-blip <borashourya609@gmail.com>
2026-06-11 15:30:48 +0530

    daily commit

--- pattern/invertedtri.cpp
+++ pattern/invertedtri.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main() {
-    int n = 5;
+    int n = 4;
 
-    for(int i = n; i >= 1; i--) {
-        for(int j = 1; j <= i; j++) {
-            cout << "*";
+    for(int i = 0; i<n ; i++) {
+        for(int j = 0 ; j<i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < n-i ; j++) {
+            cout << (n-i) ;
         }
         cout << endl;
     }
