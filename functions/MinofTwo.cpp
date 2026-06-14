d5ba138c6dddc2d5b56005f5f63d948b5a0d84a5
borashourya609-blip <borashourya609@gmail.com>
2026-06-14 17:38:40 +0530

    daily commit

---
+++ functions/MinofTwo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//min of 2 nums
+int minofTwo(int a , int b){
+    if(a<b){
+        return a;
+    } else {
+        return b;
+    }
+    
+}
+int main(){
+    cout << "min = " << minofTwo(10,9)<<endl;
+
+    return 0;
+}
