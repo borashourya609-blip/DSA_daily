8f2c043f401a4830988f4fb1c179d7bdd60d7a2b
borashourya609-blip <borashourya609@gmail.com>
2026-06-19 17:32:43 +0530

    daily commit

---
+++ functions/SumOfDigits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int sumOfDigits(int num) {
+    int digitSum = 0;
+while(num > 0){
+    int lastdig = num % 10;
+    num /= 10;
+    digitSum += lastdig;
+    }
+    return digitSum;
+}
+
+int main(){
+    cout << "sum = " << sumOfDigits(2356) << endl;
+    return 0;
+}
