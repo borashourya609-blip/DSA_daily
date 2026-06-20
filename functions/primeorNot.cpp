698d2b4f8a7a8002b8b1bb0e3d52f99170e50b25
borashourya609-blip <borashourya609@gmail.com>
2026-06-20 18:38:50 +0530

    daily commit

---
+++ functions/primeorNot.cpp
@@ -0,0 +1,30 @@
+//WAF TO CHECK IF A NUMBER IS PRIME OR NOT
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int n;
+    cout << "no. is = ";
+    cin >> n;
+   
+
+    if (isPrime(n))
+        cout << "Prime";
+    else
+        cout << "Not Prime";
+}
