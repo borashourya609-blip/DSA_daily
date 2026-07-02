28bd8c6162f709b65879b5efc924408ea84c3b6b
borashourya609-blip <borashourya609@gmail.com>
2026-07-02 16:36:36 +0530

    daily commit

---
+++ BinarySystem/binTodec.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int bintodecimal(int binNum) {
+    int ans =0, pow =1 ;// 2 ki power 0  
+        while (binNum > 0){
+            int rem = binNum % 10;
+            ans += (rem * pow);
+            binNum /= 10;
+            pow *= 2;
+    }
+    return ans; // decimal form
+}
+    int main(){
+        int binNum = 1100101;
+        cout << bintodecimal(binNum) << endl;
+
+    return 0;
+}
